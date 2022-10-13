e7e35752f322d8cd8883ce208cfd2c899d5c592f
callahanpilkington <111899960+callahanpilkington@users.noreply.github.com>
2022-10-13 11:24:09 -0500

    Update settings.py

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-snss1+sa7p6qi&b*djbm3*)(w16s2$+&!+*-&czc1f^lpioo(f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'callahanpilkington.pythonanywhere.com']
 
